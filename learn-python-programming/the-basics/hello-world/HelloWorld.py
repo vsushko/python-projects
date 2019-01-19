aa578ce7c74de1f8a3b94f790005b9e491eeeb29
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2019-01-19 15:40:55 +0300

    added hello world file

---
+++ learn-python-programming/the-basics/hello-world/HelloWorld.py
@@ -0,0 +1,12 @@
+print('hello world')
+print(1 + 2)
+print(7 * 6)
+print()
+print("The End")
+print("Hello" + " world")
+greetings = "Hello"
+name = "Bruce"
+print(greetings + name)
+
+# comment
+print(greetings + ' ' + name)
