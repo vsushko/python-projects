2578892ed3abe64cdf6b1b25796c0845525965ee
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2019-03-18 20:35:44 +0300

    intial commit

--- learn-python-programming/the-basics/hello-world/HelloWorld.py
+++ learn-python-programming/the-basics/hello-world/HelloWorld.py
@@ -1,12 +1,18 @@
-print('hello world')
-print(1 + 2)
-print(7 * 6)
-print()
-print("The End")
-print("Hello" + " world")
-greetings = "Hello"
-name = "Bruce"
-print(greetings + name)
+# greeting = "Hello"
+# name = input("Please enter your name ")
+# print(greeting + ' ' + name)
 
-# comment
-print(greetings + ' ' + name)
+splitString = "This string has been\n split over \n several\nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\5\t"
+print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh... he\'s resting"')
+
+anotherSplitString = """This string has been
+split over 
+several lines
+"""
+
+print(anotherSplitString)
