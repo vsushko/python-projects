c09da553bb8bc0e57bdf972658f8d966bef59730
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2019-04-01 11:42:07 +0300

    variables

---
+++ learn-python-programming/the-basics/hello-world/storing-items-in-variables.py
@@ -0,0 +1,18 @@
+greeting = "Vasiliy"
+
+age = 123
+print(str(age) + greeting)
+
+a = 12
+b = 12
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print()
+
+for i in range(1, b//1):
+    print(i)
