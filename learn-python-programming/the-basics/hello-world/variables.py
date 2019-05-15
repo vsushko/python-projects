2f4a51188c78f8d67c7b4c7d2262348dd61c7e4e
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2019-05-15 23:10:21 +0300

    variables

---
+++ learn-python-programming/the-basics/hello-world/variables.py
@@ -0,0 +1,18 @@
+parrot = "Norwegian Blue"
+print(parrot)
+print(parrot[3])
+print(parrot[-3])
+print(parrot[0:6])
+print(parrot[:6])
+print(parrot[6:])
+print(parrot[-4:6])
+print(parrot[0:6:2])
+
+number = "0,223,222,222,222"
+print(number[1::4])
+
+numbers = "1, 2, 3, 4, 5"
+print(numbers[0::3])
+
+today = "day"
+print("day" in today)
