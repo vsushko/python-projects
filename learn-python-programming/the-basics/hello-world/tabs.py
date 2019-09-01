26291badaa4e004b26477d98ff539b2873475880
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2019-09-01 22:45:27 +0300

    tabs

---
+++ learn-python-programming/the-basics/hello-world/tabs.py
@@ -0,0 +1,6 @@
+__author___ = 'dev'
+
+for i in range(1, 12):
+    print("No {} squared is {} and cubed is {:3}".format(i, i ** 2, i ** 4))
+    print("Calculation complete")
+    print("Try again")
