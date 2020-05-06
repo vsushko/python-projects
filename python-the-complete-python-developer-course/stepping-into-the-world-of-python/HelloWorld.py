445fe6ede65d95a9886acb19cb333bbb5eb95293
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2020-05-06 23:23:18 +0300

    completed operator precedence

---
+++ python-the-complete-python-developer-course/stepping-into-the-world-of-python/HelloWorld.py
@@ -0,0 +1,26 @@
+greeting = "Hello"
+name = input("Please enter your name ")
+print(greeting + ' ' + name)
+
+splitString = "This string has been\n split over \n several\nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\5\t"
+print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh... he\'s resting"')
+
+anotherSplitString = """This string has been \
+split over \
+several lines \
+"""
+
+print(anotherSplitString)
+
+name = "Bob"
+age = 24
+greeting = "Hello!"
+print(type(age))
+print(type(greeting))
+
+print(name + " is " + str(age) + " years old")
---
+++ python-the-complete-python-developer-course/stepping-into-the-world-of-python/operators.py
@@ -0,0 +1,20 @@
+a = 12
+b = 3
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a / b)
+print(a // b)
+print(a % b)
+
+print(a + b / 3 - 4 * 12)
+print(a + (b/3) - (4*12))
+print((((a+b)/3)-4)*12)
+
+c = a + b
+d = c / 3
+e = d - 4
+print(e * 12)
+
+print(a / (b * a) / b)
