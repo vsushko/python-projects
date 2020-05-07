bac2bb0a0820983479dfd4e7a4d69a40b536f5d5
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2020-05-07 23:17:54 +0300

    slicing

--- python-the-complete-python-developer-course/stepping-into-the-world-of-python/strings2.py
+++ python-the-complete-python-developer-course/stepping-into-the-world-of-python/strings2.py
@@ -1,7 +1,21 @@
 parrot = "Norwegian Blue"
 
-print (parrot)
-print (parrot[3])
+print(parrot[0:6])
+print(parrot[3:5])
+print(parrot[0:9])
+print(parrot[:9])
 
-for i in "we win".split(' '):
-  print(i+"\n")
+print([parrot[10:14]])
+print(parrot[10:])
+
+print(parrot[:6])
+print(parrot[6:])
+
+print(parrot[:6] + parrot[6:])
+
+print(parrot[:])
+
+print(parrot[-4:-2])
+print(parrot[-4:12])
+
+print(parrot[-14:-8])
