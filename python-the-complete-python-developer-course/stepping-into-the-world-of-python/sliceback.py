88c071833164cc70bf97c2b76a416673255847b4
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2020-05-07 23:48:13 +0300

    slicing idioms

---
+++ python-the-complete-python-developer-course/stepping-into-the-world-of-python/sliceback.py
@@ -0,0 +1,20 @@
+letters = "abcdefjhijklmnopqrstuvwxyz"
+
+backwards = letters[::-1]
+print(backwards)
+
+# create a slice that produces the caharacters qpo
+print(letters[16:13:-1])
+
+# slice the string to produce edcba
+print(letters[4::-1])
+
+# slice the string to produce the last 8 characters, in reverse order
+print(letters[:-9:-1])
+
+print(letters[-4:])
+print(letters[-1:])
+print(letters[:1])
+
+letters = ""
+print(letters[0])
--- python-the-complete-python-developer-course/stepping-into-the-world-of-python/strings2.py
+++ python-the-complete-python-developer-course/stepping-into-the-world-of-python/strings2.py
@@ -1,21 +1,12 @@
 parrot = "Norwegian Blue"
 
-print(parrot[0:6])
-print(parrot[3:5])
-print(parrot[0:9])
-print(parrot[:9])
+print(parrot[0:6:2])
+print(parrot[0:6:3])
 
-print([parrot[10:14]])
-print(parrot[10:])
+number = "9,223;372:036 854,775;807"
+separators = number[1::4]
+print(separators)
 
-print(parrot[:6])
-print(parrot[6:])
-
-print(parrot[:6] + parrot[6:])
-
-print(parrot[:])
-
-print(parrot[-4:-2])
-print(parrot[-4:12])
-
-print(parrot[-14:-8])
+values = "".join(
+    char if char not in separators else " " for char in number).split()
+print([int(val) for val in values])
