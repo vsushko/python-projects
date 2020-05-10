46b8edf0c349cd1ce637e4b1648c54eab1498f6e
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2020-05-10 15:04:55 +0300

    conditional operators

--- python-the-complete-python-developer-course/stepping-into-the-world-of-python/section4-flow-control/guessinggame.py
+++ python-the-complete-python-developer-course/stepping-into-the-world-of-python/section4-flow-control/guessinggame.py
@@ -3,9 +3,15 @@ answer = 5
 print("Please guess number between 1 and 10")
 guess = int(input())
 
-if guess < answer:
-    print("Please guess higher")
-elif guess > answer:
-    print("Please guess lower")
+if guess != answer:
+    if guess < answer:
+        print("Please guess higher")
+    else:
+        print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("Well doen, yout guessed it")
+    else:
+        print("Sorry, you have guessed correctly")
 else:
     print("You got it first time")
