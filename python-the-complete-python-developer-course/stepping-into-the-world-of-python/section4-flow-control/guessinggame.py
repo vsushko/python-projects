b1eb22606265659aad575596580a62b2d27bc9c3
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2020-05-11 14:08:20 +0300

    challenge solution

--- python-the-complete-python-developer-course/stepping-into-the-world-of-python/section4-flow-control/guessinggame.py
+++ python-the-complete-python-developer-course/stepping-into-the-world-of-python/section4-flow-control/guessinggame.py
@@ -3,7 +3,9 @@ answer = 5
 print("Please guess number between 1 and 10")
 guess = int(input())
 
-if guess != answer:
+if guess == answer:
+    print("You got it first time")
+else:
     if guess < answer:
         print("Please guess higher")
     else:
@@ -13,5 +15,3 @@ if guess != answer:
         print("Well doen, yout guessed it")
     else:
         print("Sorry, you have guessed correctly")
-else:
-    print("You got it first time")
