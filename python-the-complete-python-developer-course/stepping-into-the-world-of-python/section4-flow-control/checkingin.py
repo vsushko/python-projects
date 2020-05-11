2e323f14ec8c71b58ed7f0e50ecc5e1fec3c280e
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2020-05-12 00:20:41 +0300

    truthy values

---
+++ python-the-complete-python-developer-course/stepping-into-the-world-of-python/section4-flow-control/checkingin.py
@@ -0,0 +1,9 @@
+parrot = "Norgewian Blue"
+
+letter = input("Enter a character: ")
+
+if letter in parrot:
+    print("{} is in {}".format(letter, parrot))
+else:
+    print("I don't need that letter")
+
