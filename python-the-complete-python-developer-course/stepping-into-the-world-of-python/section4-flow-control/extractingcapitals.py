95c9878c768a75747e241999f25a568d3c138b2c
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2020-05-12 12:30:36 +0300

    for loops and assignment solution

---
+++ python-the-complete-python-developer-course/stepping-into-the-world-of-python/section4-flow-control/extractingcapitals.py
@@ -0,0 +1,9 @@
+quote = """
+Alright, but apart form the Sanitation, the Medicine, Education, Wine,
+Public Order, Irrigation, Roads, the Fresh-Water System, and Public Health,
+what have the Romans even done for us?
+"""
+
+for char in quote:
+    if char in "ABCDEFGHIJKLMNOPQURSTUVWXYZ":
+        print(char)
