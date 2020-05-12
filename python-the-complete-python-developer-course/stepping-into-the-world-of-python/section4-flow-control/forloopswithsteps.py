80750cd52b52f4d439731e04ee4a0c01ff827b81
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2020-05-12 12:41:15 +0300

    for loops with steps

---
+++ python-the-complete-python-developer-course/stepping-into-the-world-of-python/section4-flow-control/forloopswithsteps.py
@@ -0,0 +1,9 @@
+for i in range(100):
+    if i % 7 == 0:
+        print(i)
+
+for i in range(0, 101, 7):
+    print(i)
+
+for i in range(101)[::7]:
+    print(i)
---
+++ python-the-complete-python-developer-course/stepping-into-the-world-of-python/section4-flow-control/ranges.py
@@ -0,0 +1,8 @@
+for i in range(10, 15):
+    print("i is now {}".format(i))
+
+for i in range(5):
+    print("i is now {}".format(i))
+
+for i in range(10, 0, -2):
+    print("i is now {}".format(i))
