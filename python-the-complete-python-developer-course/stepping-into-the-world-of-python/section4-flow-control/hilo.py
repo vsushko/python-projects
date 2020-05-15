fd0002bdbfe19a1855a4520cfa7de52cd15f8d65
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2020-05-16 00:09:32 +0300

    hilo game

--- python-the-complete-python-developer-course/stepping-into-the-world-of-python/section4-flow-control/hilo.py
+++ python-the-complete-python-developer-course/stepping-into-the-world-of-python/section4-flow-control/hilo.py
@@ -4,17 +4,21 @@ high = 1000
 print("Please think of a number between {} and {}")
 input("Press ENTER to start")
 
-guess = 1
+guesses = 1
 while True:
+    print("\tGuessing in the range of {} to {}".format(low, high))
     guess = low + (high - low) // 2
     high_low = input("My guess is {}. Should I guess higher or lower? "
                      "Ener h or l, or c if my guess was correct"
                      .format(guess)).casefold()
     if high_low == "h":
-      # Guess highere. The low end of thee range becomes 1 greater than the guess.
+        low = guess + 1
     elif high_low == "l":
-        pass
+        high = guess - 1
     elif high_low == "c":
-        print("I got it in {} guesses!".format(guess))
+        print("I got it in {} guesses!".format(guesses))
+        break
     else:
         print("Please enter h, l or c")
+
+    guesses = guesses + 1
