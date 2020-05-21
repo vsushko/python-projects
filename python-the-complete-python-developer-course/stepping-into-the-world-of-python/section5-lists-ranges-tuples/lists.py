ca5d80be1378c1fe5da44523d400d3350f5e98ee
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2020-05-21 17:40:20 +0300

    lists in python

---
+++ python-the-complete-python-developer-course/stepping-into-the-world-of-python/section5-lists-ranges-tuples/lists.py
@@ -0,0 +1,22 @@
+#ipAddreess = input("Please enter an IP address: ")
+# print(ipAddreess.count("."))
+
+parrot_list = ["non pinin'", "no more", "a stiff", "bereft of live"]
+
+parrot_list.append("A Norwegian Blue")
+
+for state in parrot_list:
+    print("This parrot is " + state)
+
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+
+numbers = even + odd
+numbers_in_order = sorted(numbers)
+
+print(numbers_in_order)
+
+if numbers == numbers_in_order:
+    print("The lists are equal")
+else:
+    print("The lists are not equal")
