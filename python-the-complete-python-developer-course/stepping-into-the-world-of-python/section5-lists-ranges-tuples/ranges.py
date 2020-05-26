2cad71c921630b363c746061b419a3048f9592e3
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2020-05-26 23:47:11 +0300

    more about ranges

--- python-the-complete-python-developer-course/stepping-into-the-world-of-python/section5-lists-ranges-tuples/ranges.py
+++ python-the-complete-python-developer-course/stepping-into-the-world-of-python/section5-lists-ranges-tuples/ranges.py
@@ -45,3 +45,39 @@ for i in range(3, 40, 3):
     print(i)
 
 print(my_range == range(3, 40, 3))
+
+
+decimals = range(0, 100)
+my_range = decimals[3:40:3]
+print(my_range == range(3, 49, 3))
+
+print(range(0, 5, 2) == range(0, 6, 2))
+
+print(list(range(0, 5, 2)))
+print(list(range(0, 6, 2)))
+
+r = range(0, 100)
+print(r)
+
+for i in r[::-2]:
+    print(i)
+
+print('=' * 50)
+print(range(0, 100)[::-2] == range(99, 0, -2))
+
+for i in range(99, 0, -2):
+    print(i)
+
+backString = "dlrow olleh"
+print(backString[::-1])
+
+r = range(0, 10)
+for i in r[::-1]:
+    print(i)
+
+o = range(0, 100, 4)
+print(o)
+p = o[::5]
+print(p)
+for i in p:
+    print(i)
