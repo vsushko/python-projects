a5997e8bfd765475f4174df612c9a25cf403308a
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2020-05-27 23:29:51 +0300

    tuples

---
+++ python-the-complete-python-developer-course/stepping-into-the-world-of-python/section5-lists-ranges-tuples/tuple.py
@@ -0,0 +1,31 @@
+t = "a", "b", "c"
+print(t)
+
+print("a", "b", "c")
+print(("a", "b", "c"))
+
+welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
+
+bad = "Bad Company" "Bad Company", 1974
+budgie = "Nightflight", "Budgie", 1981
+imelda = "More Mayhem", "Emilda May", 2011
+metallica = "Ride the Lightning", "Metallica", 1984
+
+print(metallica)
+print(metallica[0])
+print(metallica[1])
+print(metallica[2])
+
+#metallica[0] = "Master of puppets"
+imelda = imelda[0], "Imelda May", imelda[2]
+print(imelda)
+
+metallica2 = ["Ride the lightning", "Metallica", 1984]
+
+metallica2[0] = "Master of Puppets"
+print(metallica2)
+
+title, artist, year = imelda
+print(title)
+print(artist)
+print(year)
