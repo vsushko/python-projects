901691182f5d632af03c3d1eb7f7ad1480823e99
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2020-05-29 00:03:40 +0300

    tuples

--- python-the-complete-python-developer-course/stepping-into-the-world-of-python/section5-lists-ranges-tuples/tuple.py
+++ python-the-complete-python-developer-course/stepping-into-the-world-of-python/section5-lists-ranges-tuples/tuple.py
@@ -1,31 +1,14 @@
-t = "a", "b", "c"
-print(t)
+imelda = "More Mayhem", "Emilda May", 2011, ([(
+    1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish Town Waltz")])
 
-print("a", "b", "c")
-print(("a", "b", "c"))
-
-welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
-
-bad = "Bad Company" "Bad Company", 1974
-budgie = "Nightflight", "Budgie", 1981
-imelda = "More Mayhem", "Emilda May", 2011
-metallica = "Ride the Lightning", "Metallica", 1984
-
-print(metallica)
-print(metallica[0])
-print(metallica[1])
-print(metallica[2])
-
-#metallica[0] = "Master of puppets"
-imelda = imelda[0], "Imelda May", imelda[2]
 print(imelda)
 
-metallica2 = ["Ride the lightning", "Metallica", 1984]
+imelda[3].append((5, "All For You"))
 
-metallica2[0] = "Master of Puppets"
-print(metallica2)
-
-title, artist, year = imelda
+title, artist, year, tracks = imelda
 print(title)
+tracks.append((5, "Eternity"))
 print(artist)
-print(year)
+for song in tracks:
+    tracks, title = song
+    print("\t Track number {}, Title: {}".format(tracks, title))
