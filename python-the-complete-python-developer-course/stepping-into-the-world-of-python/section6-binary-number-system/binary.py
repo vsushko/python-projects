26d7698df5521dbea758801c1d3e060e0adf38cc
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2020-06-03 23:56:32 +0300

    completed section 6

--- python-the-complete-python-developer-course/stepping-into-the-world-of-python/section6-binary-number-system/binary.py
+++ python-the-complete-python-developer-course/stepping-into-the-world-of-python/section6-binary-number-system/binary.py
@@ -1,2 +1,29 @@
 for i in range(17):
-  print ("{0:>2} in binary is {0:>08b}".format(i))
+    print("{0:>2} in binary is {0:>08b}".format(i))
+
+for i in range(17):
+    print("{0:>2} in binary is {0:>02x}".format(i))
+
+x = 0x20
+y = 0x0a
+
+print(x)
+print(y)
+print(x*y)
+
+powers = []
+
+for power in range(15, -1, -1):
+    powers.append(2 ** power)
+
+print(powers)
+
+x = int(input("Please enter a number: "))
+
+for power in powers:
+    bit = x // power
+    if bit != 0 or power == 1:
+        printing = True
+    if printing:
+        print(bit, end='')
+    x %= power
