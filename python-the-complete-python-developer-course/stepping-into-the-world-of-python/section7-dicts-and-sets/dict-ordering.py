3b1964f37d3eecec294ca875b04f3627899fa06d
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2020-06-04 23:52:42 +0300

    dict ordering

---
+++ python-the-complete-python-developer-course/stepping-into-the-world-of-python/section7-dicts-and-sets/dict-ordering.py
@@ -0,0 +1,29 @@
+numbers = {
+    1: 'one',
+    3: 'three',
+    5: 'five',
+    7: 'seven',
+    9: 'nine'
+}
+
+print("I can count odd numbers in order")
+
+for key in numbers:
+    print(numbers[key])
+
+numbers[8] = 'eight'
+numbers[2] = 'two'
+numbers[6] = 'six'
+numbers[4] = 'four'
+
+print()
+print("I can count to 9 in order")
+
+for key in numbers:
+    print(numbers[key])
+
+print()
+print("I really can")
+
+for key in sorted(numbers):
+    print(numbers[key])
